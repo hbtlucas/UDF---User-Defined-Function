b1715e00aee897f0daf8b20a1430fb1e0543093a
Herbert Lucas <140832473+hbtlucas@users.noreply.github.com>
2024-03-14 20:37:57 -0300

    Commit inicial

---
+++ UDF.sql
@@ -0,0 +1,15 @@
+create or replace function Fverificartexto(P_texto varchar(80)
+	P_car varchar(1)) returns int as 
+$$
+declare indice numeric(2);
+		total numeric(2);
+begin
+	total:=0; 
+	for indice in 1..length(P_texto)
+	loop
+		if(substring(P_texto, indice,1)=P_car) then
+		total:=total+1;
+	end if;
+	end loop;
+	return total;
+end;
